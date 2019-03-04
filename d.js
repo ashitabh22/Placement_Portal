f624491eabcf06737a9ccd7b9d192516608ea963
Govind Kumar Arora <48126302+directorbaba@users.noreply.github.com>
2019-03-04 13:35:11 +0530

    Delete d.js

--- d.js
+++
@@ -1,9 +0,0 @@
-  	function SearchSite() {
-        document.forms['search_form'].action = 'index.php?pid=search&cx=002503337570983062401:y-x0lh4xq70&ie=UTF-8&q=' + document.getElementById('q').value + '&sa=Search';       
-        document.forms['search_form'].submit();
-    }
-
-  	function customizeFont(size) {
-  	    document.getElementById("content").style.fontSize = size;
-  	}  
-
